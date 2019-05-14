b7b3d7cf94d1b92598f40ab6674360f912cd3fdd
Nikolay <n.gelemerov@devrealm.bg>
2019-05-14 19:01:12 +0300

    add passport-google-oauth20 package; add .prettierrc file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+passport.use(new GoogleStrategy());
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("Hi from my new created server!");
-});
-
 app.listen(PORT);

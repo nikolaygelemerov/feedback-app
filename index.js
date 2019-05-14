ef09124b5472116f11291cf31ec00c73397d3414
Nikolay <n.gelemerov@devrealm.bg>
2019-05-14 18:19:25 +0300

    update server response

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hi from my server!');
+app.get("/", (req, res) => {
+  res.send("Hi from my new created server!");
 });
 
 app.listen(PORT);

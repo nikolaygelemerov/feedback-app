6760c5ebbb06efbe319f9d9187429238de8ba56e
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2019-12-28 19:45:07 +0200

    pass .env.production to heroku

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,7 +5,6 @@ import * as actions from '../actions';
 
 class Payments extends Component {
   render() {
-    console.log('process.env: ', process.env);
     return (
       <StripeCheckout
         name="Emaily"
